b14ec4c451ca43d22f68a4107396a6647d900fb4
zayne29 <481617270@qq.com>
2021-03-04 16:02:38 +0800

    ttest

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -2,7 +2,7 @@ public class HiCloud {
     public static void main(String[] args) {
         System.out.println("hi,shader!!!");
         System.out.println("hi,shader!!!");
-
+        System.out.println("hi,shader!!!");
 
     }
 }
