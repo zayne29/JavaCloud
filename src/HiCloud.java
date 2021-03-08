e74b5b51634908fb9576e76254f7b55da146d422
zayne29 <481617270@qq.com>
2021-03-08 08:54:19 +0800

    ttest

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -1,7 +1,13 @@
+package HiCloud.src;
+
 public class HiCloud {
     public static void main(String[] args) {
-        System.out.println("hi,shader!!!");
+        System.out.println("zhenbeixin!!!");
         System.out.println("提交提交!!!");
-        System.out.println("三月八号背心第一次提交!!");
+        System.out.println("提交提交!!!");
+        System.out.println("提交提交!!!");
+
+
+
     }
 }
