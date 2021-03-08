3a433254843bf811caed3252666ef58b94366050
blackpinkbb <1368698567@qq.com>
2021-03-08 08:32:12 +0800

    38test1

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -2,6 +2,6 @@ public class HiCloud {
     public static void main(String[] args) {
         System.out.println("hi,shader!!!");
         System.out.println("提交提交!!!");
-        System.out.println("三月八号背心第一次提交!!!");
+        System.out.println("三月八号背心第一次提交!!");
     }
 }
