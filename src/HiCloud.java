299a5e07188e653cf95300a19dc2392cb4d8a2e8
zayne29 <481617270@qq.com>
2021-03-08 09:32:45 +0800

    hellotext1

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -3,12 +3,8 @@ package HiCloud.src;
 public class HiCloud {
     public static void main(String[] args) {
         System.out.println("zhenbeixin!!!");
-        System.out.println("zhenbeixin!!!");
-        System.out.println("提交提交!!!");
-        System.out.println("提交提交!!!");
-        System.out.println("提交提交!!!");
+        System.out.println("yaozeqin");
 
-        System.out.println("我是你爸爸!!!");
 
 
 
