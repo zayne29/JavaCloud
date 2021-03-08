3a094ea4bbc3f8202c986a7366c262103164fb87
zayne29 <481617270@qq.com>
2021-03-08 08:26:55 +0800

    ttest

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -1,7 +1,6 @@
 public class HiCloud {
     public static void main(String[] args) {
-        System.out.println("hi,shader!!!");
-        System.out.println("hi,shader!!!");
+        System.out.println("zhenbeixin!!!");
         System.out.println("提交提交!!!");
         System.out.println("提交提交!!!");
 
