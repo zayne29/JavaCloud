5675ac6ced3efc731d0d15f7282e94e5648b2b78
zayne29 <481617270@qq.com>
2021-03-08 08:59:17 +0800

    ttest

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -3,6 +3,7 @@ package HiCloud.src;
 public class HiCloud {
     public static void main(String[] args) {
         System.out.println("zhenbeixin!!!");
+        System.out.println("zhenbeixin!!!");
         System.out.println("提交提交!!!");
         System.out.println("提交提交!!!");
         System.out.println("提交提交!!!");
