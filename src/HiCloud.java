b88ab06ee06a8923475dfcce074938c6219b9aaa
zayne29 <481617270@qq.com>
2021-03-09 13:42:22 +0800

    111

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -3,6 +3,7 @@ public class HiCloud {
     public static void main(String[] args) {
         System.out.println("zhenbeixin!!!");
         System.out.println("yaozeqin");
+        System.out.println("123456");
 
     }
 }
