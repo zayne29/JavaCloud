c792ae812935a1be71845fdf1b2af848d58c65b6
blackpinkbb <1368698567@qq.com>
2021-03-09 13:28:03 +0800

    test1

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -3,7 +3,7 @@ public class HiCloud {
     public static void main(String[] args) {
         System.out.println("zhenbeixin!!!");
         System.out.println("yaozeqin");
-        System.out.println("111111");
+        System.out.println("22222");
 
 
 
