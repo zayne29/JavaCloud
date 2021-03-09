d5e580dc837f03f5fabae64c9567144312777d4c
zayne29 <481617270@qq.com>
2021-03-09 13:36:23 +0800

    second time

--- src/HiCloud.java
+++ src/HiCloud.java
@@ -4,7 +4,7 @@ public class HiCloud {
     public static void main(String[] args) {
         System.out.println("zhenbeixin!!!");
         System.out.println("yaozeqin");
-        System.out.println("上传");
+        System.out.println("上传c");
 
 
 
